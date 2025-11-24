b3f248263bc5533c04cbfffa3e2599a9d8e5aee6
bouchaiblemaireatuniv <bouchaib.lemaire@univ-paris13.fr>
2025-11-24 11:36:12 +0100

    Etape 1 calculatrice

--- calculatrice_backend/src/main/java/fr/devavance/calculatrice/Calculator.java
+++ calculatrice_backend/src/main/java/fr/devavance/calculatrice/Calculator.java
@@ -1,8 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package fr.devavance.calculatrice;
 
 /**
  *
- * @author blemaire
+ * @author marmotton
  */
 public class Calculator {
     
@@ -11,10 +15,16 @@ public class Calculator {
      * @param s_operande_1 : première opérande
      * @param s_operande_2 : seconde opérande
      * @return somme des deux opérances
+     * @throws NumberFormatException 
      */
-    public static Double addition(Integer s_operande_1, Integer s_operande_2)  {
+    public static double addition(String s_operande_1, String s_operande_2) 
+            throws NumberFormatException {
         
-        return Double.valueOf(s_operande_1 + s_operande_2);
+        Operandes operandes = convertOperandesToDouble(
+                                                    s_operande_1, 
+                                                    s_operande_2 );
+
+        return operandes.getOperande1() + operandes.getOperande2();
        
     }
 	
@@ -23,10 +33,16 @@ public class Calculator {
      * @param s_operande_1 : première opérande
      * @param s_operande_2 : seconde opérande
      * @return somme des deux opérances
+     * @throws NumberFormatException 
      */
-    public static Double soustraction(Integer s_operande_1, Integer s_operande_2) {
-            
-        return Double.valueOf(s_operande_1 - s_operande_2);
+    public static double soustraction(String s_operande_1, String s_operande_2) 
+            throws NumberFormatException {
+
+        Operandes operandes = convertOperandesToDouble(
+                                                    s_operande_1, 
+                                                    s_operande_2 );
+
+              return operandes.operande1 - operandes.operande2;
        
     }
     
@@ -35,14 +51,21 @@ public class Calculator {
      * @param s_operande_1 : première opérande
      * @param s_operande_2 : seconde opérande
      * @return somme des deux opérances
-     * @throws ArithmeticException 
+     * @throws NumberFormatException 
      */
-    public static Double division(Integer s_operande_1, 
-                                  Integer s_operande_2) {
+    public static double division(String s_operande_1, 
+                                  String s_operande_2) 
+                           throws NumberFormatException, ArithmeticException {
         
-        if (s_operande_2.intValue()==0) throw new ArithmeticException();
+        Operandes operandes = convertOperandesToDouble(
+                                                    s_operande_1, 
+                                                    s_operande_2 );
         
-        return Double.valueOf(s_operande_1 / (float) s_operande_2);
+        if (operandes.getOperande2() == 0) {
+            throw new ArithmeticException("Division par zéro !");
+        }
+
+              return operandes.operande1 / (double) operandes.operande2;
        
     }
     
@@ -53,11 +76,56 @@ public class Calculator {
      * @return somme des deux opérances
      * @throws NumberFormatException 
      */
-    public static Double multiplication(Integer s_operande_1, 
-                                        Integer s_operande_2)  {
-        return Double.valueOf(s_operande_1 * s_operande_2);
+    public static double multiplication(String s_operande_1, 
+                                        String s_operande_2) 
+                                throws NumberFormatException {
+        
+        Operandes operandes = convertOperandesToDouble(
+                                                    s_operande_1, 
+                                                    s_operande_2 );
+
+              return operandes.operande1 * operandes.operande2;
        
     }
+    
+    
+    
+    private static Operandes convertOperandesToDouble(
+                                       String s_operande_1, 
+                                       String s_operande_2) 
+                                 throws NumberFormatException{
         
+        
+        Operandes operandes = new Operandes(
+                              Integer.parseInt(s_operande_1), 
+                              Integer.parseInt(s_operande_2) 
+                                   );
+        
+        return operandes;
+    }
+
+    
+    
+    private static class Operandes {
+        
+        private int operande1;
+        private int operande2;
+
+        public Operandes(int operande1, int operande2) {
+            this.operande1 = operande1;
+            this.operande2 = operande2;
+        }
+
+        public int getOperande1() {
+            return operande1;
+        }
+
+        public int getOperande2() {
+            return operande2;
+        }
+        
+        
+    }
+            
     
 }
--- calculatrice_backend/src/main/java/fr/devavance/calculatrice/controller/CalculatorController.java
+++ calculatrice_backend/src/main/java/fr/devavance/calculatrice/controller/CalculatorController.java
@@ -1,6 +1,10 @@
 package fr.devavance.calculatrice.controller;
 
-
+/*
+ * Click nbfs:nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs:nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+import fr.devavance.calculatrice.Operation;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -10,116 +14,156 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import fr.devavance.calculatrice.Calculator;
-
-
-import fr.devavance.calculatrice.exceptions.OperatorException;
-
-import java.util.ArrayList;
+import fr.devavance.calculatrice.Operation;
 
 
 /**
  *
  * @author B. LEMAIRE
  * Controller pour la calculatrice
- * <p>
- * Ce code utilise volontairement des anti-patterns, il n'a pas un
- * bon "good smell"
- * Ce code doit être refactorisé  pour respecter les
- * principes du "good smell code"
  */
-
-
+ 
+// Pour serveur Tomcat de démonstration
+//@WebServlet(urlPatterns = {"/calculate/etape_1/*"}) 
 @WebServlet(urlPatterns = {"/calculate/*"})
 public class CalculatorController extends HttpServlet {
-
-    private static ArrayList<String> permittedOperators = null;
-
-
-    @Override
-    public void init() throws ServletException {
-        super.init();
-        this.permittedOperators = new ArrayList<>();
-
-        this.permittedOperators.add("add");
-        this.permittedOperators.add("sub");
-        this.permittedOperators.add("mul");
-        this.permittedOperators.add("div");
-
-
-    }
-
+    
     //<editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request  servlet request
+     * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException      if an I/O error occurs
+     * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response)
+    protected void doGet(HttpServletRequest request, 
+            HttpServletResponse response)
             throws ServletException, IOException {
-
-
-        String op = request.getParameter("operation");
-        String operande1 = request.getParameter("operande1");
-        String operande2 = request.getParameter("operande2");
-
-        if (op == null
-                || op.isEmpty()
-                || !this.permittedOperators.contains(op))
-
-            throw new OperatorException();
-
-
-        int op1 = Integer.parseInt(operande1);
-        int op2 = Integer.parseInt(operande2);
-
-
-        double r;
-
-        if (op.equals("add"))
-            r = Calculator.addition(op1, op2);
-        else if (op.equals("sub"))
-            r = Calculator.soustraction(op1, op2);
-        else if (op.equals("div"))
-            r = Calculator.division(op1, op2);
-        else if (op.equals("mul"))
-            r = Calculator.multiplication(op1, op2);
-        else throw new ServletException("Opération invalide !");
-
-
-        response.setContentType("text/html;charset=UTF-8");
-
-        PrintWriter out = response.getWriter();
-        try {
-
-
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Calculator</title>");
-            out.println("</head>");
-            out.println("<body>");
-
-            out.println("<div>");
-            out.println("<p class=\"operande\">Operande 1 : " + op1 + "</p>");
-            out.println("<p class=\"operande\">Operande 2 : " + op2 + "</p>");
-            out.println("<p class=\"operation\">Operateur : " + op + "</p>");
-            out.println("<p class=\"resultat\">resultat : " + r + "</p>");
-            out.println("</div>");
-
-            out.println("</body>");
-            out.println("</html>");
-        } finally {
-            out.close();
-
-
-        }
+       
+        Operation operation = new Operation();
+  
+        extractParamsFromURLParamaters(request, operation);
+      
+        checkIfAllElementsOfOperationNotEmpty(operation);
+        
+        proceedOperation(operation);
+        
+        proceedView(response, operation);
+     
 
     }
-}
+    
+    
+    private void extractParamsFromURLParamaters(HttpServletRequest request, 
+            Operation operation){
+        
+        String operator= request.getParameter("operation");
+        String operande1= request.getParameter("operande1");
+        String operande2= request.getParameter("operande2");
+        
+        operation.setOperator(operator);
+        operation.setOperande1(operande1);
+        operation.setOperande2(operande2);
+    }
+    
+    
+    private void checkIfAllElementsOfOperationNotEmpty(Operation operation) 
+            throws ServletException{
+        
+        String operator= operation.getOperator();
+        String operande1= operation.getOperande1();
+        String operande2= operation.getOperande2();
+                         
+        if (operator==null 
+            || operator.isEmpty() 
+            || operande1 == null
+            || operande1.isEmpty()
+            || operande2 == null 
+            || operande2.isEmpty()
+         ) 
+        throw new ServletException("Format de l'opérationi invalide !");
+        
+    }
+    
+    
+    
+     private void proceedOperation(Operation operation) 
+            throws ServletException{
+            
+        String operator= operation.getOperator();
+        String operande1= operation.getOperande1();
+        String operande2= operation.getOperande2();
+        
+        double result;
+            
+        
+         try {            
+            if (operator.equals("add"))
+                result = Calculator.addition(operande1, 
+                        operande2);
+            else if (operator.equals("sub"))
+                result = Calculator.soustraction(operande1, 
+                        operande2);
+            else if (operator.equals("div"))
+                result = Calculator.division(operande1, 
+                        operande2);
+            else if (operator.equals("mul"))
+                result = Calculator.multiplication(operande1, 
+                        operande2);
+            else throw new Exception("Opération invalide !");
+        }
+        catch(ArithmeticException e ){
+            throw new ServletException(e);
+        }
+        catch (NumberFormatException e) {
+               throw new ServletException(e);
+        }
+        catch (Exception e) {
+               throw new ServletException(e);
+        }       
+   
+        operation.setResult(result);
+        
+    }
      
+    public void proceedView(HttpServletResponse response, Operation operation) 
+            throws IOException{
+        
+           
+        String operator= operation.getOperator();
+        String operande1= operation.getOperande1();
+        String operande2= operation.getOperande2();
+        double result = operation.getResult();
+        
+               
+        response.setContentType("text/html;charset=UTF-8");
+        
+                PrintWriter out = response.getWriter();
+        try {
+
+       
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Calculator</title>");
+        out.println("</head>");
+        out.println("<body>");
+
+        out.println("<div>");
+        out.println("<p class=\"operande\">Operande 1 : " + operande1+"</p>");
+        out.println("<p class=\"operande\">Operande 2 : " + operande2+"</p>");
+        out.println("<p class=\"operation\">Operateur : " + operator+"</p>");
+        out.println("<p class=\"resultat\">resultat : " + result +"</p>");
+        out.println("</div>");
+      
+        out.println("</body>");
+        out.println("</html>");
+        }
+        finally{
+        out.close();
+        }
+    }
+    
+}
