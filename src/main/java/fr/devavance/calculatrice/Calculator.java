323b35bbd4d75b814b83f6f99a7a9c526c362cfe
bouchaiblemaireatuniv <bouchaib.lemaire@univ-paris13.fr>
2025-11-24 12:00:43 +0100

    Etape 1 calculatrice demo

--- src/main/java/fr/devavance/calculatrice/Calculator.java
+++ src/main/java/fr/devavance/calculatrice/Calculator.java
@@ -17,11 +17,14 @@ public class Calculator {
      * @return somme des deux opérances
      * @throws NumberFormatException 
      */
-    public static double addition(String s_operande_1, String s_operande_2) throws NumberFormatException {
-        int operande_1 = Integer.parseInt(s_operande_1);        
-        int operande_2 = Integer.parseInt(s_operande_2);
+    public static double addition(String s_operande_1, String s_operande_2) 
+            throws NumberFormatException {
+        
+        Operandes operandes = convertOperandesToDouble(
+                                                    s_operande_1, 
+                                                    s_operande_2 );
 
-        return operande_1 + operande_2;
+        return operandes.getOperande1() + operandes.getOperande2();
        
     }
 	
@@ -32,11 +35,14 @@ public class Calculator {
      * @return somme des deux opérances
      * @throws NumberFormatException 
      */
-    public static double soustraction(String s_operande_1, String s_operande_2) throws NumberFormatException {
-        int operande_1 = Integer.parseInt(s_operande_1);        
-        int operande_2 = Integer.parseInt(s_operande_2);
+    public static double soustraction(String s_operande_1, String s_operande_2) 
+            throws NumberFormatException {
 
-        return operande_1 - operande_2;
+        Operandes operandes = convertOperandesToDouble(
+                                                    s_operande_1, 
+                                                    s_operande_2 );
+
+              return operandes.operande1 - operandes.operande2;
        
     }
     
@@ -47,11 +53,19 @@ public class Calculator {
      * @return somme des deux opérances
      * @throws NumberFormatException 
      */
-    public static double division(String s_operande_1, String s_operande_2) throws NumberFormatException, ArithmeticException {
-        int operande_1 = Integer.parseInt(s_operande_1);        
-        int operande_2 = Integer.parseInt(s_operande_2);
+    public static double division(String s_operande_1, 
+                                  String s_operande_2) 
+                           throws NumberFormatException, ArithmeticException {
+        
+        Operandes operandes = convertOperandesToDouble(
+                                                    s_operande_1, 
+                                                    s_operande_2 );
+        
+        if (operandes.getOperande2() == 0) {
+            throw new ArithmeticException("Division par zéro !");
+        }
 
-        return operande_1 / operande_2;
+              return operandes.operande1 / (double) operandes.operande2;
        
     }
     
@@ -62,12 +76,56 @@ public class Calculator {
      * @return somme des deux opérances
      * @throws NumberFormatException 
      */
-    public static double multiplication(String s_operande_1, String s_operande_2) throws NumberFormatException {
-        int operande_1 = Integer.parseInt(s_operande_1);        
-        int operande_2 = Integer.parseInt(s_operande_2);
+    public static double multiplication(String s_operande_1, 
+                                        String s_operande_2) 
+                                throws NumberFormatException {
+        
+        Operandes operandes = convertOperandesToDouble(
+                                                    s_operande_1, 
+                                                    s_operande_2 );
 
-        return operande_1 * operande_2;
+              return operandes.operande1 * operandes.operande2;
        
     }
+    
+    
+    
+    private static Operandes convertOperandesToDouble(
+                                       String s_operande_1, 
+                                       String s_operande_2) 
+                                 throws NumberFormatException{
+        
+        
+        Operandes operandes = new Operandes(
+                              Integer.parseInt(s_operande_1), 
+                              Integer.parseInt(s_operande_2) 
+                                   );
+        
+        return operandes;
+    }
+
+    
+    
+    private static class Operandes {
+        
+        private int operande1;
+        private int operande2;
+
+        public Operandes(int operande1, int operande2) {
+            this.operande1 = operande1;
+            this.operande2 = operande2;
+        }
+
+        public int getOperande1() {
+            return operande1;
+        }
+
+        public int getOperande2() {
+            return operande2;
+        }
+        
+        
+    }
+            
     
 }
