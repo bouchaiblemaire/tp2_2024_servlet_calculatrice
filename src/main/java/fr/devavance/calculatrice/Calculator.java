a49dbedf8731798090c3e09e54ef8d454bb454d2
bouchaiblemaireatuniv <bouchaib.lemaire@univ-paris13.fr>
2025-11-24 12:04:57 +0100

    Etape1 calculatrice corrige

--- src/main/java/fr/devavance/calculatrice/Calculator.java
+++ src/main/java/fr/devavance/calculatrice/Calculator.java
@@ -1,12 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package fr.devavance.calculatrice;
 
 /**
- * @author BL
- * Calculatrice
+ *
+ * @author marmotton
  */
 public class Calculator {
     
-
+    /**
+     * Addition
+     * @param s_operande_1 : première opérande
+     * @param s_operande_2 : seconde opérande
+     * @return somme des deux opérances
+     * @throws NumberFormatException 
+     */
     public static double addition(String s_operande_1, String s_operande_2) 
             throws NumberFormatException {
         
@@ -18,6 +28,13 @@ public class Calculator {
        
     }
 	
+    /**
+     * Soustraction
+     * @param s_operande_1 : première opérande
+     * @param s_operande_2 : seconde opérande
+     * @return somme des deux opérances
+     * @throws NumberFormatException 
+     */
     public static double soustraction(String s_operande_1, String s_operande_2) 
             throws NumberFormatException {
 
@@ -29,6 +46,14 @@ public class Calculator {
        
     }
     
+     /**
+     * Division
+     * @param s_operande_1 : première opérande
+     * @param s_operande_2 : seconde opérande
+     * @return somme des deux opérances
+     * @throws NumberFormatException 
+     * @throws ArithmeticException
+     */
     public static double division(String s_operande_1, 
                                   String s_operande_2) 
                            throws NumberFormatException, ArithmeticException {
@@ -45,7 +70,13 @@ public class Calculator {
        
     }
     
-
+     /**
+     * Multiplication
+     * @param s_operande_1 : première opérande
+     * @param s_operande_2 : seconde opérande
+     * @return somme des deux opérances
+     * @throws NumberFormatException 
+     */
     public static double multiplication(String s_operande_1, 
                                         String s_operande_2) 
                                 throws NumberFormatException {
@@ -53,23 +84,28 @@ public class Calculator {
         Operandes operandes = convertOperandesToDouble(
                                                     s_operande_1, 
                                                     s_operande_2 );
-        
+
               return operandes.operande1 * operandes.operande2;
        
     }
+    
+    
     
     private static Operandes convertOperandesToDouble(
                                        String s_operande_1, 
                                        String s_operande_2) 
                                  throws NumberFormatException{
         
+        
         Operandes operandes = new Operandes(
                               Integer.parseInt(s_operande_1), 
                               Integer.parseInt(s_operande_2) 
                                    );
+        
         return operandes;
     }
 
+    
     
     private static class Operandes {
         
@@ -89,6 +125,8 @@ public class Calculator {
             return operande2;
         }
         
-    }
         
+    }
+            
+    
 }
--- src/main/java/fr/devavance/calculatrice/controller/CalculatorController.java
+++ src/main/java/fr/devavance/calculatrice/controller/CalculatorController.java
@@ -1,6 +1,10 @@
 package fr.devavance.calculatrice.controller;
 
-
+/*
+ * Click nbfs:nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs:nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+import fr.devavance.calculatrice.Operation;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -19,9 +23,20 @@ import fr.devavance.calculatrice.Operation;
  * Controller pour la calculatrice
  */
  
+// Pour serveur Tomcat de démonstration
+//@WebServlet(urlPatterns = {"/calculate/etape_1/*"}) 
 @WebServlet(urlPatterns = {"/calculate/*"})
 public class CalculatorController extends HttpServlet {
-
+    
+    //<editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, 
             HttpServletResponse response)
@@ -37,6 +52,7 @@ public class CalculatorController extends HttpServlet {
         
         proceedView(response, operation);
      
+
     }
     
     
